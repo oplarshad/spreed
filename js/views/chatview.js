1f20f39cff94cf7d778b3df79e3c55fdc95606fe
Daniel Calviño Sánchez <danxuliu@gmail.com>
2018-10-18 17:51:07 +0200

    Keep a reference to the newest comment added
    This avoids iterating over all the comments each time a new one is
    added.
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- js/views/chatview.js
+++ js/views/chatview.js
@@ -240,6 +240,8 @@
 		onRender: function() {
 			delete this._lastAddedMessageModel;
 
+			this._$newestComment = $();
+
 			this.$el.find('.emptycontent').after(this.addCommentTemplate({}));
 
 			this.$el.find('.has-tooltip').tooltip({container: this._tooltipContainer});
@@ -392,11 +394,11 @@
 		_onAddModel: function(model, collection, options) {
 			this.$el.find('.emptycontent').toggleClass('hidden', true);
 
-			var $newestComment = this.$container.children('.comment').last();
-			var scrollToNew = $newestComment.length > 0 && this._getCommentTopPosition($newestComment) < this.$container.outerHeight();
+			var scrollToNew = this._$newestComment.length > 0 && this._getCommentTopPosition(this._$newestComment) < this.$container.outerHeight();
 
 			var $el = $(this.commentTemplate(this._formatItem(model)));
 			this.$container.append($el);
+			this._$newestComment = $el;
 
 			if (this._modelsHaveSameActor(this._lastAddedMessageModel, model) &&
 					this._modelsAreTemporaryNear(this._lastAddedMessageModel, model) &&
@@ -426,7 +428,7 @@
 			this._postRenderItem(model, $el);
 
 			if (scrollToNew) {
-				var newestCommentHiddenHeight = (this._getCommentTopPosition($el) + this._getCommentOuterHeight($el)) - this.$container.outerHeight();
+				var newestCommentHiddenHeight = (this._getCommentTopPosition(this._$newestComment) + this._getCommentOuterHeight(this._$newestComment)) - this.$container.outerHeight();
 				this.$container.scrollTop(this.$container.scrollTop() + newestCommentHiddenHeight);
 			}
 		},
